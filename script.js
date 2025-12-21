411b17147fc3234f0072e0d80f7723f17570e81c
RCM <189871873+rcmbcs@users.noreply.github.com>
2025-12-21 15:49:33 +0000

    Battersea Power Station marker

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const map = L.map('map').setView([51.4821, -0.1440], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+// Battersea Power Station Market
+L.marker([51.4821, -0.1440])
+    .addTo(map)
+    .bindPopup("Battersea Power Station Market")
+    .openPopup();
+
